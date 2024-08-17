1bae36764e130d45584d5c2eec45fc4accab6815
Guilherme de Souza <guilhermesp.prof@gmail.com>
2024-08-17 19:33:31 -0300

    Deixando um jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];   
-let numeroLimite = 100;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAletorio();
 let tentativas = 1;
 function exibirTextoNaTela(tag,texto){    
